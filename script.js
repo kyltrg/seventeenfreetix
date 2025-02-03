53b8cbb366a32413e3c46461b09fa349110078b3
kyltrg <tarogkyle1@gmail.com>
2025-02-04 00:31:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,47 @@
-"use strict";
+let clickCount = 0;
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const text = document.getElementById("text");
+const image = document.getElementById("image");
 
-const titleElement = document.querySelector(".title");
-const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.querySelector(".btn--yes");
-const noButton = document.querySelector(".btn--no");
-const image = document.querySelector(".image");
-
-const MAX_IMAGES = 5;
-const imageUrls = [
-  "please_1.jpg", 
-  "please_2.jpg", 
-  "please_3.jpg", 
-  "please_4.jpg", 
-  "please_5.jpg"
+const images = [
+    "img/please_1.jpg", "img/please_2.jpg", "img/please_3.jpg", "img/please_4.jpg", "img/please_5.jpg", "img/YEYYY.jpg"
 ];
-const finalImage = "YEYYY.jpg";
 
-let play = true;
-let noCount = 0;
-
-yesButton.addEventListener("click", handleYesClick);
-
-noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
-    }
-  }
-});
-
-function handleYesClick() {
-  titleElement.innerHTML = "YEYYY!!!";
-  buttonsContainer.classList.add("hidden");
-  changeImage(finalImage);
-  playVoiceMessage();
-}
-
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
-
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
-function generateMessage(noCount) {
-  const messages = [
-    "No",
+const texts = [
+    "Loveee Leila, will u be my valentine?",
     "Are you sure love?",
     "Aww love naman, sure??",
     "DON'T DO THIS TO MEEEE!!!!",
-    "PLEASEEE LOVE LEILAAA!!! HUHUHU"
-  ];
+    "PLEASEEE LOVE LEILAAA!!! HUHUHU",
+    "YEYYY!!!"
+];
 
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
-}
+// Function to handle Yes button clicks
+yesBtn.addEventListener("click", function() {
+    if (clickCount === 5) {
+        image.src = images[5];
+        text.innerHTML = texts[5];
+        yesBtn.style.width = "100vw";
+        yesBtn.style.height = "100vh";
+        noBtn.style.display = "none"; // Hide 'No' button when 'Yes' is clicked
+        // Play voice message
+        const audio = new Audio('your-audio-file.mp3');
+        audio.play();
+    } else {
+        clickCount++;
+        yesBtn.style.transform = `scale(${1 + clickCount * 0.5})`;  // Only scale the Yes button
+        text.innerHTML = texts[clickCount];
+        image.src = images[clickCount];
+        // Adjust 'No' button text after each click
+        if (clickCount < 5) {
+            noBtn.innerHTML = texts[clickCount + 1] || "Can you please click Yes?";
+        }
+    }
+});
 
-function changeImage(image) {
-  if (image === finalImage) {
-    image.src = `img/${image}`;
-  } else {
-    image.src = `img/${imageUrls[image - 1]}`;
-  }
-}
-
-function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
-}
-
-function playVoiceMessage() {
-  const audio = new Audio('voice-message.mp3');
-  audio.play();
-}
+// Function to handle No button clicks
+noBtn.addEventListener("click", function() {
+    // Increase 'Yes' button size on 'No' click
+    yesBtn.style.transform = `scale(${1 + clickCount * 0.5})`;
+});
