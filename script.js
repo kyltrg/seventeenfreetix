2d5207390407f4ba79073d9977412c1b0b23c8e5
kyltrg <tarogkyle1@gmail.com>
2025-02-04 01:49:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 "use strict";
 
+// Selecting DOM elements
 const titleElement = document.querySelector(".title");
 const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.getElementById("yes");
-const noButton = document.getElementById("no");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
 const catImg = document.querySelector(".cat-img");
 
 const MAX_IMAGES = 5;
 
 let play = true;
 let noCount = 0;
-let yesScale = 1; // Track scale of the "Yes" button
+let yesScale = 1; // Initial scale for the Yes button
 
+// Event listeners
 yesButton.addEventListener("click", handleYesClick);
-
 noButton.addEventListener("click", function () {
   if (play) {
     noCount++;
     const imageIndex = Math.min(noCount, MAX_IMAGES);
     changeImage(imageIndex);
+    resizeYesButton();
     updateNoButtonText();
     if (noCount === MAX_IMAGES) {
       play = false;
@@ -30,45 +32,49 @@ function handleYesClick() {
   titleElement.innerHTML = "Yayyy!! :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
-  enlargeYesButton();
+  enlargeYesButton(); // Enlarge the Yes button
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
 }
 
 function enlargeYesButton() {
-  // If we are at the final "I'm gonna cry..." stage, enlarge the Yes button to full screen
-  if (noCount >= MAX_IMAGES) {
-    yesButton.classList.add("full-screen-btn");
+  yesScale *= 1.2; // Increase the scale by 1.2 each time
+  yesButton.style.transform = `scale(${yesScale})`;
+
+  if (yesScale >= 4) { // When it reaches scale 4, make the button full screen
+    yesButton.style.width = "100vw";
+    yesButton.style.height = "100vh";
+    yesButton.style.fontSize = "5rem"; // Make the font larger for full-screen button
+    yesButton.style.display = "flex";
+    yesButton.style.alignItems = "center";
+    yesButton.style.justifyContent = "center";
+    yesButton.style.backgroundColor = "#40c057"; // Make it green for the final screen
     yesButton.innerHTML = "YES!!!";
-    yesButton.style.fontSize = "6rem"; // Larger font size
-  } else {
-    yesScale *= 1.6; // Increase scale factor
-    yesButton.style.transform = `scale(${yesScale})`; // Apply scale transformation
-    const computedStyle = window.getComputedStyle(yesButton);
-    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-    const newFontSize = fontSize * 1.6; // Increase font size proportionally
-    yesButton.style.fontSize = `${newFontSize}px`; // Apply new font size
   }
 }
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
-    "Can you please click Yes?",
-    "Are you sure love?",
-    "Aww love naman, sure?? :((",
-    "DON'T DO THIS TO MEEEE!!!!",
-    "I'm gonna cry...",
+    "No", // First No message
+    "Can you please click Yes?", // Second No message
+    "Are you sure love?", // Third No message
+    "Aww love naman, sure?? :(((", // Fourth No message
+    "DON'T DO THIS TO MEEEE!!!!", // Fifth No message
+    "I'm gonna cry...", // Sixth No message
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
   return messages[messageIndex];
 }
 
-function changeImage(imageIndex) {
-  if (imageIndex === "yes") {
-    catImg.src = `img/YEYYY.jpg`; // Final "yes" image
-  } else {
-    catImg.src = `img/please_${imageIndex}.jpg`; // Custom images for each "No"
-  }
+function changeImage(image) {
+  catImg.src = `img/${image}.jpg`;
 }
 
 function updateNoButtonText() {
